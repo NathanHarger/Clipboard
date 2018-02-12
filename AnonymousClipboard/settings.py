1e38af4285c5f4c4c328170aa9b7bc4144400f0a
nathan <nathan.j.harger@gmail.com>
2018-02-12 15:01:41 -0800

    Add 127.0.0.1 to allowed hosts

--- AnonymousClipboard/settings.py
+++ AnonymousClipboard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^+ysa($tct+alt(d@3_t+hx#bnx*aiow%0tup0-$5h^it)qlnt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["testserver",'localhost']
+ALLOWED_HOSTS = ["testserver",'localhost','127.0.0.1']
 
 
 # Application definition
