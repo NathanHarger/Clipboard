4c41fed40094556662deef941013eb4ff8873ae3
nathan <nathan.j.harger@gmail.com>
2018-02-22 17:34:50 -0800

    commit changes before npm eject

--- AnonymousClipboard/settings.py
+++ AnonymousClipboard/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'webpack_loader',
 ]
 
 MIDDLEWARE = [
@@ -53,11 +54,16 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'AnonymousClipboard.urls'
-
+WEBPACK_LOADER = {
+    'DEFAULT': {
+            'BUNDLE_DIR_NAME': 'bundles/',
+            'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.dev.json'),
+        }
+}
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates"), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- frontend/urls.py
+++ frontend/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
 from . import views
+from django.conf.urls import url
+from django.contrib import admin
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
 	
 	path('index/',views.index,name="index"),
-	
+	url(r'^', TemplateView.as_view(template_name="index.html")),
+
 
 ]
