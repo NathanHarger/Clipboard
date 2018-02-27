a6f7881a1e75b1df389152198235a1441a7ea2b9
nathan <nathan.j.harger@gmail.com>
2018-02-26 21:47:56 -0800

    Initial code

--- AnonymousClipboard/settings.py
+++ AnonymousClipboard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^+ysa($tct+alt(d@3_t+hx#bnx*aiow%0tup0-$5h^it)qlnt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["testserver",'localhost','127.0.0.1']
+ALLOWED_HOSTS = ["testserver",'localhost','127.0.0.1','dry-harbor-54108.herokuapp.com']
 
 
 # Application definition
@@ -133,6 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 MEDIA_URL = '/uploads/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads/')
 
