922829bf4ee92ccaa563a7888d8ce81058a3ec02
nathan <nathan.j.harger@gmail.com>
2018-02-27 19:45:04 -0800

    Added django-cors-headers

--- AnonymousClipboard/settings.py
+++ AnonymousClipboard/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'webpack_loader',
 ]
@@ -50,12 +51,19 @@ MIDDLEWARE = [
 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:3000',
+)
+
 CSRF_COOKIE_NAME = "XSRF-TOKEN"
 
 ROOT_URLCONF = 'AnonymousClipboard.urls'
