3ab3a7204cd72f1a4c86e9c52d01e55d2d27d570
nathan <nathan.j.harger@gmail.com>
2018-03-01 14:21:33 -0800

    Add correct message to print

--- clipboard/views.py
+++ clipboard/views.py
@@ -163,7 +163,7 @@ def respond_as_attachment(request, file_path, original_filename, file_format):
     
  
     s3 = boto3.resource('s3')
-    print(s3.get_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=original_filename))
+    print(s3.Bucket(settings.AWS_STORAGE_BUCKET_NAME).Object(Key=original_filename).get())
     def generate():
         with s3.Bucket(settings.AWS_STORAGE_BUCKET_NAME).Object(Key=original_filename).get()["Body"] as f:
             yield from f
