3b45fd61c1711df883389baca828248c991a85f8
NathanHarger <nathan.j.harger@gmail.com>
2018-03-01 14:40:50 -0800

    Update views.py

--- clipboard/views.py
+++ clipboard/views.py
@@ -164,7 +164,7 @@ def respond_as_attachment(request, file_path, original_filename, file_format):
  
     s3 = boto3.resource('s3')
     def generate():
-        with s3.Bucket(settings.AWS_STORAGE_BUCKET_NAME).Object(Key=media+'/'+original_filename).get()['Body'].read() as f:
+        with s3.Bucket(settings.AWS_STORAGE_BUCKET_NAME).Object(Key='/media/'+original_filename).get()['Body'].read() as f:
             yield from f
         default_storage.delete(file_path)
 
