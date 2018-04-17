721a7fc878dd19d4e85984064b33e962233f7fae
Nathan Harger <nathan.j.harger@gmail.com>
2018-04-16 17:55:42 -0700

    Update settings.py

--- AnonymousClipboard/settings.py
+++ AnonymousClipboard/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ["testserver",'localhost','127.0.0.1','localhost:3000','localhost:5000','vast-chamber-77416.herokuapp.com']
 
 
